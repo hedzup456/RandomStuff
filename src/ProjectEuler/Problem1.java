e30a1a6fb72fa935c406eec277534dd757a64336
Richard Henry <richardhenry602@gmail.com>
2015-09-01 12:38:33 +0100

    First commit - Project Euler Problem 1

---
+++ src/ProjectEuler/Problem1.java
@@ -0,0 +1,11 @@
+package ProjectEuler;
+
+public class Problem1 {
+	public static void main(String[] args){
+		int runningTotal = 0;
+		for(int i = 0; i<=1000; i++){
+			runningTotal = (i%3==0||i%5==0)?runningTotal+i:runningTotal;
+		}// /for
+		System.out.println(runningTotal);
+	} // /main
+} // /class
