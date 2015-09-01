accd1c7705c87f8886b211d66c5b75d7842f8ee9
Richard Henry <richardhenry602@gmail.com>
2015-09-01 20:54:29 +0100

    Problem 4

---
+++ src/ProjectEuler/Problem4.java
@@ -0,0 +1,23 @@
+package ProjectEuler;
+
+public class Problem4 {
+	public static boolean numTest(int number){
+		String reverse = "";
+		String string = Integer.toString(number);
+		int length = string.length();
+		for (int i = length-1; i>=0; i--){
+			reverse=reverse+string.charAt(i);
+		} // for
+		return (string.equals(reverse))?true:false;
+	} // numTest
+	
+	public static void main(String[] args){
+		int ans = 0;
+		for(int a=100;a < 1000; a++){
+			for(int b=100;b < 1000; b++){
+				ans = (numTest(a*b) && ans<a*b)?a*b:ans;
+			} // for b
+		} // for a
+		System.out.println(ans);
+	} // main
+} // Class
