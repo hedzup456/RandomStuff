b33368322c4d046e4efdc9464e982510308ad1d8
Richard Henry <richardhenry602@gmail.com>
2015-09-01 20:19:13 +0100

    Problem 3

---
+++ src/ProjectEuler/Problem3.java
@@ -0,0 +1,17 @@
+package ProjectEuler;
+
+public class Problem3 {
+	public static int primeFactor(long number){
+		int i; // So declared out of loop so returnd out of loop
+		for (i=2; i<=number; i++){
+			if (number % i ==0){
+				number = number / i;
+				i--;
+			} //if
+		} // for
+		return i;
+	} // primeFactor
+	public static void main(String[] args){
+		System.out.println(primeFactor(600851475143L));
+	} // main
+} // class
