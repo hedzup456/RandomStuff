bdc9dbf997f04af2d111f6e518ef1cfb42a41f8f
Richard Henry <richardhenry602@gmail.com>
2015-09-14 14:18:15 +0100

    Problem 9 solved.

---
+++ src/ProjectEuler/Problem9.java
@@ -0,0 +1,33 @@
+package ProjectEuler;
+
+public class Problem9 {
+	public static void pythagTrip(){
+		int m, n, a = 0, b = 0, c = 0, sum = 0;
+		int product = 0;
+		
+		for (m=2; m < 100; m++){
+			for (n = 1; n < 100; n++){
+				while(m > n){
+					a = (m*m) - (n*n);
+					b = (2*m) * n;
+					c = (m*m) + (n*n);
+					
+					sum = a + b + c;
+					
+					if(sum==1000){
+						product = a*b*c;
+						
+						System.out.println(a + ", " + b + ", " + c);
+						System.out.println(product);
+						break;
+					} // if
+					break;
+				}// while
+			} // n for
+		} // m for
+	} //pythagTrip
+
+	public static void main(String[] args) {
+		pythagTrip();
+	} // Main
+} // Class
