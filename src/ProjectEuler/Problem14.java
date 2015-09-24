0bddd072918e0f7f324c88f6fb54bf1e104870f6
Richard Henry <richardhenry602@gmail.com>
2015-09-24 10:42:41 +0100

    Problem 14 solved... Eventually.

--- src/ProjectEuler/Problem14.java
+++ src/ProjectEuler/Problem14.java
@@ -1,5 +1,6 @@
 package ProjectEuler;
 
+
 public class Problem14 {
 	public static int collatz(int seed){
 		int count = 1; // init count at one as there will always be at least on number in the chain
@@ -15,17 +16,32 @@ public class Problem14 {
 		return count;
 	} // collatz
 	
+	
+	public static int collatz2(long seed){
+		int count = 0;
+		while(seed > 1){
+			System.out.print(seed + " ");
+			if (seed%2==1) seed = 3*seed+1;
+			else seed /= 2;
+			count++;
+		} // while
+		count++;
+		System.out.print(seed);
+		System.out.println();
+		return count;
+	} // collatz2
+	
 	public static void main(String[] args){
-		int bestSeed = -1, lengthChain = 0, longestChain = 0;
-		for (int newSeed = 1; newSeed < 1000000; newSeed++){
-			lengthChain = collatz(newSeed);
+		long bestSeed = -1, lengthChain = 0, longestChain = 0;
+		for (long newSeed = 1; newSeed < 1000000; newSeed++){
+			lengthChain = collatz2(newSeed);
 			if (lengthChain > longestChain){
 				longestChain = lengthChain;
 				bestSeed = newSeed;
-				System.out.println(bestSeed);
 			} // if
 		} // for
 		
 		System.out.println(bestSeed);
+		
 	} // main
 } // class
