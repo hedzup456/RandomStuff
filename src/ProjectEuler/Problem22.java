38a2e6c408c2cbd2ce6e9d958c51a9633e2ee0ec
Richard <richardhenry602@gmail.com>
2015-10-02 21:24:19 +0100

    Problem 22. Depends on net connection and github file p22_names.txt

--- src/ProjectEuler/Problem22.java
+++ src/ProjectEuler/Problem22.java
@@ -1,35 +1,46 @@
 package ProjectEuler;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Problem22 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String fileName = "C:\\Users\\Richard\\workspace\\RandomStuff\\src\\ProjectEuler\\p022_names.txt";
+		String fileAddr= "https://raw.githubusercontent.com/hedzup456/RandomStuff/master/src/ProjectEuler/p22_names.txt";
 		String line = null;
 		
 		
 		try{
-			FileReader fileReader = new FileReader(fileName);
+			URL url = new URL(fileAddr);
+			Scanner s = new Scanner(url.openStream());
 			
-			BufferedReader bufferedReader = new BufferedReader(fileReader);
-			line = bufferedReader.readLine();
-			String[] names = line.split("\",\"");
-			
+			line = s.nextLine();
+			line = line.replaceAll("\"", "");
+			String[] names = line.split(",");
+			System.out.println("Got text");
 			Arrays.sort(names);
+
+			s.close();
 			
-			for(int i = 0; i < names.length; i++) System.out.println(names[i]);
-			
-			bufferedReader.close();
-		} catch (FileNotFoundException e){
-			System.out.println("Can't find");
-		} catch (IOException ex){
-			System.out.println("Can't read");
+			long total = 0;
+			for(int i = 0; i < names.length; i ++){
+				int nameScore = 0;
+				for(int j = 0; j < names[i].length(); j++){
+					char x = names[i].charAt(j);
+					nameScore += CommonMethods.charVal(x);
+				} // for j
+				nameScore = nameScore * (i+1);
+				total += nameScore;
+			} // for i
+			System.out.println(total);
+		} catch (MalformedURLException e){
+			System.out.println("Oops.");
+		} catch (IOException e){
+			System.out.println("Oops.");
 		}
 
 	}
