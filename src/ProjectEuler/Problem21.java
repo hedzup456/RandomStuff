72a80617d5de7f8a78371cc48adce7817d341fdb
Richard Henry <richardhenry602@gmail.com>
2015-10-02 15:47:06 +0100

    Problem 21 solved.

--- src/ProjectEuler/Problem21.java
+++ src/ProjectEuler/Problem21.java
@@ -3,17 +3,27 @@ package ProjectEuler;
 public class Problem21 {
 	private static int sumOfDivisors(int seed){
 		int total = 0;
-		for(int i = 1; i < (seed/2); i++){
-			if (seed%i == 0) total += i;
+		for(int i = 1; i <= (seed/2); i++){
+			if (seed%i == 0){
+				total += i;
+			}
 		}
 		return total;
 	} // sumOfDivisors
+	
+	private static boolean isAmicable(int number){
+		int sOD1 = sumOfDivisors(number);
+		int sOD2 = sumOfDivisors(sOD1);
+	
+		if (sOD2 == number && sOD1 != sOD2) return true;
+		return false;
+	} // isAmicable
+	
 	public static void main(String[] args) {
 		int total = 0;
-		for(int number = 1; number < 10000; number++){
-			int sOD = sumOfDivisors(number);
-			if
+		for (int i = 1; i < 10000; i++){
+			if (isAmicable(i)) total += i;
 		}
-	}
-
-}
+		System.out.println(total);
+	} // main
+} // class
