c8ad33f05d3a912c25ae9fd4df732757228b6196
Richard Henry <richardhenry602@gmail.com>
2015-10-06 21:38:45 +0100

    ~~

--- src/ProjectEuler/Problem23.java
+++ src/ProjectEuler/Problem23.java
@@ -5,9 +5,28 @@ public class Problem23 {
 		if (CommonMethods.sumOfDivisors(seed) > seed) return true;
 		return false;
 	}
-	public static void main(String[] args) {
+	private static boolean[] genAbun(){
+		boolean[] arr = new boolean[23123];
+		for (int i = 0; i < 23123; i++){
+			if (isAbundant(i+1)) arr[i] = true;
+		}
+		return arr;
+	}
+	private static int[] niceArray(boolean[] arr) {
 		
-
+		return new int[1];
+	}
+	
+	public static void main(String[] args) {
+		int total = 276;
+		for(int tot = 25; tot < 28124; tot++){
+			for(int num1 = 12; num1 < tot; num1++){
+				for (int num2 = 12; num2 < tot; num2++){
+					if(!(num1+num2 == tot && isAbundant(num1) && isAbundant(num2))) total += tot;
+				}
+			} // num 1 for
+		} // tot for
+		System.out.println(total);
 	}
 
 }
