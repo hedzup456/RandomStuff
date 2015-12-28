170932006ea32a31a05c344ffee6b893de66bd72
Richard Henry <richardhenry602@gmail.com>
2015-12-28 19:36:59 +0000

    Short addition to the WithWillow code files

---
+++ src/WithWillow/HelloName.java
@@ -0,0 +1,12 @@
+package WithWillow;
+
+import java.util.Scanner;
+
+public class HelloName {
+	public static void main(String[] args){
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("What's your name?");
+		String name = scanner.nextLine();
+		System.out.println("Hello " + name);
+	}
+}
