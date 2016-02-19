4c7c216a8c4118fdf62cf5d73efd119729da5124
Richard Henry <richardhenry602@gmail.com>
2016-02-19 20:44:28 +0000

    various activities

---
+++ src/ProjectEuler/Problem38.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package ProjectEuler;
+
+/**
+ * @author 	Richard
+ * @since	19 Feb 2016
+ *
+ */
+public class Problem38 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
