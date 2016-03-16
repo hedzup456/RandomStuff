9b87e7eb772baca4e43204220149de98eccd4d81
Richard Henry <richardhenry602@gmail.com>
2016-03-16 20:25:40 +0000

    Added some JavaDoc, among other things

--- src/dailyProgrammer/challenge258.java
+++ src/dailyProgrammer/challenge258.java
@@ -5,100 +5,156 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.SocketException;
-import java.util.Random;
 import java.util.Scanner;
 
+/**
+ * 
+ * @author 		Richard Henry (richardhenry602@gmail.com)
+ * @version 	0.2
+ * @since		2016-03-16
+ */
 public class challenge258 {
-	public static String nick = "hedzup456_bot";	// My name, but I'm  a bot
-	public static String user = nick + " 0 * :" + nick;	// ain't none of y'all getting my real name 
-	public static String server = "chat.freenode.net"; 
-	public static int serverPort = 6667;
-	public static String channel = "#reddit-dailyprogrammer";
+	//	User info
+	private static String nick = "hedzup456_bot";	// My name, but I'm  a bot
+	private static String user = nick + " 0 * :" + nick;	// ain't none of y'all getting my real name 
+	//	Server Info
+	private static String server = "chat.freenode.net"; 
+	private static int serverPort = 6667;
+	private static String channel = "#reddit-dailyprogrammer";
 	
-	public static DataInputStream inStrm;
-	public static DataOutputStream outStrm;
-	public static Socket socket;
+	//	Input/Output
+	//	Declared now, filled later to allow multi-method access
+	private static DataInputStream inStrm;
+	private static DataOutputStream outStrm;
+	private static Socket socket;
 	
-	public static final String PrivKey = genPK();
+	private static final String eln =  "\r\n";	// IRC Endline. eln is easier to remember than \r\n. I guarantee I'd forget \r\n.
 	
-	
-	public static final String eln =  "\r\n";	// IRC Endline
-	
-	@SuppressWarnings("deprecation")
-	public static void main(String[] args) {
+	/**
+	 * Simple method to connect from to the server at the given address and port.
+	 * Outputs to standard out the status of some stuff.
+	 * 
+	 * @param server	The server to connect to
+	 * @param port		The port to connect over.
+	 * 
+	 * @return Boolean to say whether the method has connected.
+	 */
+	private static boolean connect(String server, int port){
+		System.out.println("Proceeding to open a socket and data streams.");
 		try{
-			boolean connectedToChannel = false;
-			System.out.println("Creating Socket");
-			socket = new Socket(server, serverPort);
+			socket = new Socket(server, port);
 			inStrm = new DataInputStream(socket.getInputStream());
 			outStrm = new DataOutputStream(socket.getOutputStream());
-			System.out.println("Socket and data streams created.");
 			
-			Scanner sc = new Scanner(System.in);
-			
-			outStrm.writeBytes("NICK " + nick + eln);	// NICK connection
-			outStrm.writeBytes("USER " + user + eln);	// USER connection message
-			outStrm.writeBytes("JOIN " + channel + eln);
-			
-			String incoming = "";
-			while ((incoming = inStrm.readLine()) != null) {	// While we get things
-				//	From server
-				if(incoming.contains("hedzbot")){
-					parseCommand(incoming, PrivKey);
-				} else if(incoming.contains("PING :")){	// Deal with PING keepalives
-					incoming = incoming.substring(6);	// Ignore the "PING :" bit
-					outStrm.writeBytes("PONG :" + incoming + eln);
-					System.out.println("Just got pinged. Dealt with it, though");
-				} else System.out.println(neaten(incoming, connectedToChannel));
-				connectedToChannel = (connectedToChannel || incoming.toLowerCase().contains("end of /names list"));
-			}
-			//	Close stuff off, just for neatness.
-			sc.close();
-			socket.close();
-			inStrm.close();
-			outStrm.close();
+			System.out.println("Sockets created.");
+			return true;
 		} catch (SocketException e){
 			e.printStackTrace();
 		} catch (IOException e){
 			e.printStackTrace();
 		}
+		return false;
 	}
-	private static String genPK(){
-		String k =Integer.toHexString(new Random().nextInt(100000));
-		if (k.length() < 6){
-			k = k + (6-k.length())*'1';
-		}
-		System.out.println("____PRIVATE KEY____");
-		System.out.println(k);
-		System.out.println();
-		return k;		
-	}
-	private static void quit(){
+	/**
+	 * Method to connect using nick and user data defined in global variables above.
+	 * 
+	 * @since 2016-03-16
+	 * @return void
+	 */
+	private static void joinServerAndChannel(){
 		try{
+			outStrm.writeBytes("NICK " + nick + eln);	// NICK connection
+			outStrm.writeBytes("USER " + user + eln);	// USER connection message
+			outStrm.writeBytes("JOIN " + channel + eln);	// Join channel	
+		} catch (IOException e){
+			e.printStackTrace();
+		}
+	}
+	/**
+	 * Method to quit, disconnecting each of the streams and so on.
+	 * <p>
+	 * The idea behind this is that it makes disconnecting from a server much neater; call a method
+	 * and that's it. None of this ugly force-killing of tasks or whatever.
+	 * 
+	 * @since 2016-03-16
+	 * @return void
+	 */
+	private static void quit(){
+		// Each thing is in it's own try/catch because I want to be sure each thing gets closed properly.
+		// Nothing in catches - I'm not too fussed if an exception doesn't get handled at this point.
+		try{	// Actually try to disconnect from any server - neatly.
 			outStrm.writeBytes("QUIT" + eln);
 		} catch (NullPointerException e){
-			// Do nothing
-		} catch (IOException e){}
-		try{
-			socket.close();
-		} catch (Exception e) {}
-		try{
+		}catch (IOException e){}
+		try{	// Stab it through the ears
 			inStrm.close();
 		} catch (Exception e) {}
-		try{
+		try{	// Cut out it's tongue
 			outStrm.close();
-		} catch (Exception e) {}		
+		} catch (Exception e) {}
+		try{	// Murder it in cold blood
+			socket.close();
+		} catch (Exception e) {}
 	}
-	private static void parseCommand(String rawCommand, String privKey){
-		if (rawCommand.contains("disconnect")){
-			if (rawCommand.contains(privKey)) quit();
+	/**
+	 * Method to parse a command received by a user with !hedzbot
+	 * <p>
+	 * Method only really looks in the message for certain keywords.
+	 *   
+	 * @param rawCommand
+	 * @return void
+	 */
+	private static void parseCommand(String rawCommand){
+		if (rawCommand.contains("disconnect") || rawCommand.contains("quit") || rawCommand.contains("kill") || rawCommand.contains("seppuku")){
+			quit();
 		}
 	}
-	private static String neaten(String rough, boolean ctc){
-		if (rough.contains("@") && ctc){	// String is likely a user if it contains '@'. Must be connected to channel if you want formatting.
-			StringBuilder neat = new StringBuilder(rough.substring(1));	// Noone wants the leading colon
-			neat = neat.delete(neat.indexOf("!"), neat.indexOf(" "));	// Hopefully strips out the address/hostname.
-			return neat.toString();
-		} else return rough;	// Might be important 
+	/**
+	 * Method to strip out unnecessary text from messages - ultimately there should just be the username and their message.
+	 * 
+	 * @param rough A line of raw text from the server.
+	 * @return a neatened version of the entered text.
+	 */
+	private static String neaten(String rough){
+		try{
+			if (rough.contains("@")){	// String is likely a user if it contains '@'.
+				String[] parts = rough.split(" ");
+				if(parts.length > 3){
+					parts[0] = parts[0].substring(0, parts[0].indexOf('!'));	// Drop the hostname and other boring stuff like that
+					parts[3] = parts[3].substring(1);	//	Drop the colon before the actual message
+					String neat = parts[0] + ": ";
+					for(int i = 3; i < parts.length; i++)	neat = neat + parts[i] + " ";	// Add the rest of the message. Because the whole point of it is to output the message.
+					return neat;
+				} else return rough;
+			} else return rough;	// Might be important
+		} catch (StringIndexOutOfBoundsException e){
+			return rough;	// Something fucked up. I can't be bothered to work out what.
+		}
+	}
+	
+	@SuppressWarnings("deprecation")	//	DataInputStream readLine is depreciated. I might even fix it at some point.
+	public static void main(String[] args) {
+		try{
+			connect(server, serverPort);	
+			Scanner sc = new Scanner(System.in);
+			
+			joinServerAndChannel();
+
+			String incoming = "";
+			while ((incoming = inStrm.readLine()) != null) {	// While we get things
+				//	From server
+				if(incoming.contains("!hedzbot")){
+					parseCommand(incoming);
+				} else if(incoming.contains("PING :")){	// Deal with PING keepalives
+					incoming = incoming.substring(6);	// Ignore the "PING :" bit
+					outStrm.writeBytes("PONG :" + incoming + eln);
+					System.out.println("Just got pinged. Dealt with it, though");
+				} else System.out.println(neaten(incoming.substring(1)));
+			}
+			quit();	// Disconnect from the server.
+			sc.close();
+		} catch (IOException e){
+			e.printStackTrace();
+		}
 	}
 }
